4c065cbe69f114d80d3e033c684d8ae4c63657dc
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2025-03-17 11:46:16 +0100

    Add new Index class. Rename file

--- mediasort.py
+++ mediasort.py
@@ -6,11 +6,35 @@ import hashlib
 import ffmpeg
 import mimetypes
 import shutil
+import logging
+from dataclasses import dataclass
+from collections import defaultdict
 from pathlib import Path
 from datetime import datetime
 from tqdm import tqdm
 from PIL import Image, ExifTags
 
+
+def get_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        console_handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "{asctime} - {levelname} - {message}",
+            style="{",
+            datefmt="%Y-%m-%d %H:%M",
+        )
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+    return logger
+
+logger = get_logger()
+
+
+###############################################################################
+#                               FileDescriptor                                #
+###############################################################################
 
 def file_type(path: Path | str) -> str:
     mime_type, _ = mimetypes.guess_type(str(path))
@@ -50,17 +74,27 @@ def datetime_from_path(path: str):
     return None
 
 
-
+@dataclass
 class FileDescriptor:
-    def __init__(self, path: Path | str):
-        self.path = str(path)
+    path: str
+    size: int
+    hash: str
+    typ: str
+    _meta: dict | None = None
+
+    @staticmethod
+    def from_file(path: Path | str):
+        path = str(path)
         with open(path, "rb") as fp:
             content = fp.read()
-        self.size = len(content)
-        self.hash = hashlib.sha1(content).hexdigest()
-        self.typ = file_type(path)
-        self._image_meta = None
-        self._video_meta = None
+        size = len(content)
+        hash = hashlib.sha1(content).hexdigest()
+        typ = file_type(path)
+        return FileDescriptor(path=path, size=size, hash=hash, typ=typ)
+
+    @staticmethod
+    def from_dict(dct: dict):
+        return FileDescriptor(**dct)
 
     def __repr__(self):
         return f"FileDescriptor(path='{self.path}', size={self.size})"
@@ -70,33 +104,19 @@ class FileDescriptor:
         return os.path.basename(self.path)
 
     @property
-    def image_meta(self):
-        if self.typ != "image":
-            return None
-        if self.size == 0:
-            self._image_meta = {}
-        if self._image_meta is None:
-            self._image_meta = image_metadata(self.path)
-        return self._image_meta
-
-    @property
-    def video_meta(self):
-        if self.typ != "video":
-            return None
-        if self.size == 0:
-            self._video_meta = {}
-        if self._video_meta is None:
-            self._video_meta = video_metadata(self.path)
-        return self._video_meta
-
-    @property
     def meta(self):
-        if self.typ == "image":
-            return self.image_meta
-        elif self.typ == "video":
-            return self.video_meta
-        else:
-            return None
+        if self._meta is None:
+            if self.typ == "image":
+                self._meta = image_metadata(self.path)
+            elif self.typ == "video":
+                self._meta = video_metadata(self.path)
+            # otherwise: keep _meta == None
+        return self._meta
+
+    def is_same(self, other):
+        self_dt = self.record_time or datetime(1, 1, 1)
+        other_dt = other.record_time or datetime(1, 1, 1)
+        return self.hash == other.hash and self_dt.year == other_dt.year and self_dt.month == other_dt.month
 
     @property
     def record_time(self):
@@ -127,9 +147,9 @@ class FileDescriptor:
         return None
 
 
-def _same_file(fd1: FileDescriptor, fd2: FileDescriptor):
-    return fd1.hash == fd2.hash and fd1.record_time == fd2.record_time
-
+###############################################################################
+#                               find_issues                                   #
+###############################################################################
 
 def find_issues(index: list[FileDescriptor]):
     hashes = {}
@@ -142,7 +162,7 @@ def find_issues(index: list[FileDescriptor]):
             continue
         if fd.hash in hashes:
             existing_fd = hashes[fd.hash]
-            if _same_file(existing_fd, fd):
+            if existing_fd.is_same(fd):
                 duplicates.append((existing_fd, fd))
             else:
                 collisions.append((existing_fd, fd))
@@ -155,19 +175,71 @@ def find_issues(index: list[FileDescriptor]):
     }
 
 
-def build_index(root: Path | str):
-    root = Path(root).expanduser()
-    index = []
-    pbar = tqdm(sorted(root.rglob("*")))
-    for path in pbar:
-        if path.is_file():
-            fd = FileDescriptor(path)
-            index.append(fd)
-        relpath = str(path.relative_to(root))
-        relpath = relpath.rjust(30)[-30:]
-        pbar.set_description(relpath)
-    return index
+###############################################################################
+#                                    Index                                    #
+###############################################################################
 
+class CollisionException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class Index:
+    def __init__(self, cachefile: str | None = None, recreate=False):
+        self.cachefile = cachefile or os.path.abspath("media-index.jsonl")
+        if recreate and os.path.exists(self.cachefile):
+            os.remove(self.cachefile)
+        self.items = []
+        self.hashes = defaultdict(lambda: [])    # hash -> [fd]
+        self.paths = defaultdict(lambda: [])     # path -> fd
+        self.update_from_cache(self.cachefile)
+
+    def __repr__(self):
+        return f"Index(cachefile='{self.cachefile}', len={len(self.items)})"
+
+    def update_from_cache(self, cachefile: str):
+        logger.info(f"Pre-loading index from {cachefile}")
+        if os.path.exists(cachefile):
+            with open(cachefile) as fp:
+                for line in fp:
+                    dct = json.loads(line)
+                    fd = FileDescriptor.from_dict(dct)
+                    self.add(fd)
+
+    def add(self, fd: FileDescriptor):
+        existing = self.hashes[fd.hash]
+        if existing and any(fd.path == e.path for e in existing):
+            # file already added to the index
+            return False
+        self.items.append(fd)
+        self.hashes[fd.hash].append(fd)
+        self.paths[fd.path].append(fd)
+        with open(self.cachefile, "a") as fp:
+            fp.write(json.dumps(fd.__dict__) + "\n")
+        return True
+
+    def update(self, root: Path | str):
+        root = Path(root).expanduser()
+        pbar = tqdm(sorted(root.rglob("*")))
+        for path in pbar:
+            if path.is_file() and str(path) not in self.paths:
+                fd = FileDescriptor.from_file(path)
+                self.add(fd)
+            relpath = str(path.relative_to(root))
+            relpath = relpath.rjust(30)[-30:]
+            pbar.set_description(relpath)
+
+
+def test_index():
+    self = Index(recreate=True)
+    fd = FileDescriptor.from_file('/Users/az/ElementsBackup/GooglePhotos/photos/2016/04/IMG_3770.JPG')
+    assert self.add(fd) == True
+    assert self.add(fd) == False
+    self.update("~/ElementsBackup")
+
+###############################################################################
+#                                 reorganize                                  #
+###############################################################################
 
 def maybe_increment_path(path: str):
     if not os.path.exists(path):
@@ -200,27 +272,27 @@ def copy_with_retry(src, dst, n_retries=10):
         raise ValueError(f"Failed to copy {src} -> {dst} after 10 retries")
 
 
-class ProgressLogger:
-    def __init__(self, log_file: str):
-        self.log_file = log_file
-        self.records = []
-        self.record_set = set([])
-        if os.path.exists(log_file):
-            with open(log_file) as fp:
-                for line in fp:
-                    rec = json.loads(line)
-                    self.records.append(rec)
-                    self.record_set.add((rec["src"], rec["dst"]))
+# class ProgressLogger:
+#     def __init__(self, log_file: str):
+#         self.log_file = log_file
+#         self.records = []
+#         self.record_set = set([])
+#         if os.path.exists(log_file):
+#             with open(log_file) as fp:
+#                 for line in fp:
+#                     rec = json.loads(line)
+#                     self.records.append(rec)
+#                     self.record_set.add((rec["src"], rec["dst"]))
 
-    def exists(self, src: str, dst: str):
-        return (src, dst) in self.record_set
+#     def exists(self, src: str, dst: str):
+#         return (src, dst) in self.record_set
 
-    def log(self, src: str, dst: str):
-        rec = {"src": src, "dst": dst}
-        self.records.append(rec)
-        self.record_set.add((src, dst))
-        with open(self.log_file, "a") as fp:
-            fp.write(json.dumps(rec) + "\n")
+#     def log(self, src: str, dst: str):
+#         rec = {"src": src, "dst": dst}
+#         self.records.append(rec)
+#         self.record_set.add((src, dst))
+#         with open(self.log_file, "a") as fp:
+#             fp.write(json.dumps(rec) + "\n")
 
 
 def reorganize(src: str | list[str], dest: str):
@@ -232,13 +304,14 @@ def reorganize(src: str | list[str], dest: str):
         root = Path(root).expanduser()
         idx = build_index(root)
         index.extend(idx)
-    dest_index = build_index(dest)
-    dest_hash2path = {fd.hash : fd.path for fd in dest_index}
     print("Analyzing for issues")
     issues = find_issues(index)
     duplicates = set(fd.path for _, fd in issues["duplicates"])
     collisions = set(fd.path for _, fd in issues["collisions"])
     print(f"Found {len(duplicates)} duplicates and {len(collisions)} collisions")
+    print("Indexing destination")
+    dest_index = build_index(dest)
+    dest_hash2path = {fd.hash : fd.path for fd in dest_index}
     print(f"Files already in destination: {len([fd for fd in index if fd.hash in dest_hash2path])}")
     print(f"Copying to the {dest}")
     dest = os.path.expanduser(dest).rstrip("/")
@@ -283,3 +356,5 @@ def main():
     # src = "~/Takeout"
     # dest = "~/TakeoutReorganized"
     reorganize(src, dest)
+
+    # TODO: create index cache, don't re-index destination
